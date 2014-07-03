49aa03b32f970852bef356ce8beeeb55bc390c5c
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2014-07-03 22:10:09 +0200

    rename of ApiModels to Models

---
+++ FxSyncNet/Models/CryptoKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FxSyncNet.ApiModels
+{
+    public class CryptoKeys
+    {
+        public string Collection { get; set; }
+        public string[] Default { get; set; }
+    }
+}
---
+++ FxSyncNet/Models/KeysResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FxSyncNet.ApiModels
+{
+    public class KeysResponse
+    {
+        public string Bundle { get; set; }
+    }
+}
