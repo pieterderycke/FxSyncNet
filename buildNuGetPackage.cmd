7572010581ea6206f68624e375864c8419cb98f5
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2014-07-28 15:23:16 +0200

    Added a nuget package buildscript

---
+++ buildNuGetPackage.cmd
@@ -0,0 +1,21 @@
+SET version="0.1"
+
+msbuild /p:Configuration=Release FxSyncNet\FxSyncNet.csproj
+MKDIR nuget\lib\net45
+COPY FxSyncNet\bin\Release\FxSyncNet.dll nuget\lib\net45\
+
+msbuild /p:Configuration=Release FxSyncNet.Universal\FxSyncNet.Universal.csproj
+MKDIR nuget\lib\wpa
+COPY FxSyncNet.Universal\bin\Release\FxSyncNet.dll nuget\lib\wpa\
+COPY FxSyncNet.Universal\bin\Release\JWT.Universal.dll nuget\lib\wpa\
+
+msbuild /p:Configuration=Release FxSyncNet.Universal\FxSyncNet.Universal.csproj
+MKDIR nuget\lib\windows8
+COPY FxSyncNet.Universal\bin\Release\FxSyncNet.dll nuget\lib\windows8\
+COPY FxSyncNet.Universal\bin\Release\JWT.Universal.dll nuget\lib\windows8\
+
+COPY FxSyncNet.nuspec nuget\
+
+Tools\NuGet\nuget.exe pack nuget\FxSyncNet.nuspec -Version %version%
+
+RMDIR nuget /S /Q
